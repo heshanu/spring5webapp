5b611ff067f3381a2a618ffedac8a7aca857c500
heshan umayanga <heshanu97@gmail.com>
2022-03-29 00:07:06 +0530

    pushing added book set

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,11 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.Set;
+
 public class Author {
 
-    private
+    private String firstName;private String lastName;private Set<Book> books;
+
+    public Author() {
+    }
 }
